2bc8d27b764c399e9155000c41c48ecaee3869a3
Anas Elsheikh <141143774+anas-204@users.noreply.github.com>
2025-08-12 18:18:05 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,20 +1,21 @@
 interface Teacher {
-  readonly firstName : string
-  readonly lastName : string
-  fullTimeEmployee : boolean
-  yearsOfExperience : number
-  location : string
-  [key : string] : any
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;  
+  location: string;           
+  yearsOfExperience?: number; 
+  [key: string]: any;         
 }
-interface directors extends Teacher{
-  numberOfReports : number
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  const firstInitial = firstName.length > 0 ? firstName[0].toUpperCase() : '';
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
   return `${firstInitial}. ${lastName}`;
-}
+};
