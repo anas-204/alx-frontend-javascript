c6e6ae6a531b4cc834f69eba6363e904de3045db
Anas Elsheikh <141143774+anas-204@users.noreply.github.com>
2025-08-12 18:25:15 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,6 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
-}
+const printTeacher = function({firstName, lastName}: {firstName: string, lastName: string}): string {
+  return `${firstName[0]}. ${lastName}`;
+};
