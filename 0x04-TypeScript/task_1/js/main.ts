5ad5eaac367baf68b755e07421be2c0c5617e3dd
Anas Elsheikh <141143774+anas-204@users.noreply.github.com>
2025-08-12 18:33:03 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,33 @@ interface printTeacherFunction {
 function printTeacher({ firstName, lastName }): ({firstName: string, lastName: string}){
   return `${firstName}. ${lastName}`;
 };
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
